74753f8b336b891e1c3c5dcd10f763e905554661
elina <elinapark0818@gmail.com>
2022-01-03 22:20:23 +0900

    Add WithInteractions at PureInboxScreen

--- src/components/PureInboxScreen.js
+++ src/components/PureInboxScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { PureInboxScreen } from "./InboxScreen";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   component: PureInboxScreen,
   title: "PureInboxScreen",
